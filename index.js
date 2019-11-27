6f2abd16f61aa28b7e7dc88621e60b6463a804ff
fischer <pedro.fischer22@gmail.com>
2019-11-26 22:19:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+  
+app.use(express.json())
+
+const users = [];
+
+app.get('/', (req, res) => {
+    res.json('Api aula de segurança da informação V1.0');
+});
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send("Usuário criado com sucesso!")
+  } catch {
+    res.status(500).send("Ops..., houve um erro!")
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send('Usuário não localizado');
+  }
+  try {
+    if(await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Bem vindo!' + user.name)
+    } else {
+      res.send('Por favor, verifique suas credênciais!');
+    }
+  } catch {
+    res.status(500).send()
+  }
+})
+
+app.listen(3000)
