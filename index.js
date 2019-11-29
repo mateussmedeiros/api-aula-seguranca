b4dd8359aa199a827cb56eadf4b80fa5575c3fd1
fischer <pedro.fischer22@gmail.com>
2019-11-28 22:56:43 -0300

    [update]

--- index.js
+++ index.js
@@ -14,19 +14,15 @@ app.use(express.json())
 
 const users = [];
 
-
-
 app.get('/perfil', (req, res) => {
   res.sendFile('perfil.html', { root: path.join(__dirname, './view') });
 });
-
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, './view') });
 });
 app.get('/users', (req, res) => {
   res.json(users)
 })
-
 app.get('/register', (req, res) => {
   res.sendFile('register.html', { root: path.join(__dirname, './view') });
 });
@@ -35,18 +31,18 @@ app.post('/register', async (req, res) => {
   try {
     const user = { name: req.body.name, password: req.body.password, email:req.body.email }
     
-    if ( users.find(user => user.email === req.body.email) ) {
-      res.status(400);
+    let checkUser = users.find(user => user.email === req.body.email)  
+    
+    if(checkUser) {
+      res.status(400).send("400");
       return false;
     }
-
+    
     users.push(user)
     res.status(201).send("201");
 
   } catch (err) {
-    
     res.status(500).send("500");
- 
   }
 });
 
@@ -65,9 +61,7 @@ app.post('/login', async (req, res) => {
       res.send('Por favor, verifique suas credênciais!');
     }
   } catch (err) {
-
     res.status(500);
-  
   }
 });
 
