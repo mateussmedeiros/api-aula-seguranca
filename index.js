73c04bb92ba1f0334902f1d69f1ff3d20010dd82
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2019-12-02 21:44:06 -0300

    add nodemailer

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-app = express()
+  app = express()
 const bodyParser = require('body-parser')
 const path = require('path');
 const lowdb = require('lowdb');
@@ -10,7 +10,32 @@ const db = low(adapter);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-db.defaults({ users:[] }).write();
+db.defaults({ users: [] }).write();
+
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '',
+    pass: ''
+  }
+})
+
+var mailOptions = {
+  from: 'My Name <my.email@gmail.com>',
+  to: 'receiver.email@gmail.com',
+  subject: 'Nodemailer test',
+  text: 'Hello World!!'
+}
+
+transporter.sendMail(mailOptions, function (err, res) {
+  if (err) {
+    console.log('Error');
+  } else {
+    console.log('Email Sent');
+  }
+})
 
 
 const PORT = process.env.PORT || 3000;
@@ -23,7 +48,9 @@ app.use(function (req, res, next) {
 });
 
 
-
+app.get('/email', (req, res) => {
+  res.sendFile('email.html', { root: path.join(__dirname, './view') });
+});
 
 app.get('/perfil', (req, res) => {
   res.sendFile('perfil.html', { root: path.join(__dirname, './view') });
@@ -45,48 +72,48 @@ app.get('/register', (req, res) => {
   res.sendFile('register.html', { root: path.join(__dirname, './view') });
 });
 
-app.post('/register' , async (req, res) => {
-  
-  if(req.body.name == "" || req.body.email == "" || req.body.password == "")
-    return res.send({'data':'preencha todos os campos', 'status':'400'}).status(400);
+app.post('/register', async (req, res) => {
+
+  if (req.body.name == "" || req.body.email == "" || req.body.password == "")
+    return res.send({ 'data': 'preencha todos os campos', 'status': '400' }).status(400);
 
 
   const found = db.get('users')
-                  .find({ 
-                    email:req.body.email 
-                  })
-                  .value();
-  if(found)
-    return res.send({'data':'email já em uso no sistema', 'status':'400'}).status(400)
-                  
-    db.get('users')
+    .find({
+      email: req.body.email
+    })
+    .value();
+  if (found)
+    return res.send({ 'data': 'email já em uso no sistema', 'status': '400' }).status(400)
+
+  db.get('users')
     .push({
-      name : req.body.name,
-      email : req.body.email,
-      password : req.body.password 
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
     })
     .write();
 
-    return res.send({'data':'criado com sucesso', 'status':'201'}).status(201);
-  
- 
+  return res.send({ 'data': 'criado com sucesso', 'status': '201' }).status(201);
+
+
 });
 
 app.post('/login', async (req, res) => {
- 
-  if(req.body.name == "" || req.body.email == "" || req.body.password == "")
-    return res.send({'data':'preencha todos os campos','status':'400'}).status(400);
+
+  if (req.body.name == "" || req.body.email == "" || req.body.password == "")
+    return res.send({ 'data': 'preencha todos os campos', 'status': '400' }).status(400);
 
   const found = db.get('users')
-                  .find({ 
-                    email:req.body.email ,
-                    password:req.body.password
-                  })
-                  .value();
-  if(!found) 
-    return res.send({'data':'usuário não encontrado','status':'404'}).status(404);
+    .find({
+      email: req.body.email,
+      password: req.body.password
+    })
+    .value();
+  if (!found)
+    return res.send({ 'data': 'usuário não encontrado', 'status': '404' }).status(404);
 
- return res.send({'data':'login autorizado', 'user':{name:found.name, email:found.email}}).status(200);
+  return res.send({ 'data': 'login autorizado', 'user': { name: found.name, email: found.email } }).status(200);
 });
 
 app.listen(PORT)
